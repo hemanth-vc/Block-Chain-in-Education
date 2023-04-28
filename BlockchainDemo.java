e96b9b98daf206d420fe13fe9e41b84495cd6ade
hemanth-vc <37353727+hemanth-vc@users.noreply.github.com>
2023-04-28 14:45:08 -0600

    Added test files that mimic the transcripts

--- BlockchainDemo.java
+++ BlockchainDemo.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.util.Arrays;
 
 import pkg.*;
@@ -5,14 +6,24 @@ import pkg.*;
 public class BlockchainDemo {
 
 public static void main(String[] args) {
-    Blockchain blockchain = new Blockchain();
-    blockchain.addBlock(Arrays.asList("Test Transaction 1".getBytes(), "Test Transaction 1.1".getBytes()));
-    blockchain.addBlock(Arrays.asList("Test Transaction 2".getBytes()));
-    blockchain.addBlock(Arrays.asList("Test Transaction 3".getBytes()));
 
-    System.out.println("Blocks hash:");
-    for(Block block : blockchain.getBlocks()) {
-        System.out.println("block #" + block.getIndex()  + ": " + block.getHash());
+    try {
+        Blockchain blockchain = new Blockchain();
+        FileInputStream transcript1 = new FileInputStream("General/smiley.jpg");
+        byte[] transcript1Bytes = transcript1.readAllBytes();
+        blockchain.addBlock(Arrays.asList(transcript1Bytes, transcript1Bytes));
+        blockchain.addBlock(Arrays.asList("Test Transaction 1".getBytes(), "Test Transaction 1.1".getBytes()));
+        blockchain.addBlock(Arrays.asList("Test Transaction 2".getBytes()));
+        blockchain.addBlock(Arrays.asList("Test Transaction 3".getBytes()));
+    
+        System.out.println("Blocks hash:");
+        for(Block block : blockchain.getBlocks()) {
+            System.out.println("block #" + block.getIndex()  + ": " + block.getHash());
+        }
+    
+    } catch (Exception e) {
+        System.out.println("Exception: " + e.getMessage());
+        e.printStackTrace();
     }
 }
 }
